db6dd5e521fd8c5595997646ab026217f2ac54da
Mhaylson Whandson <mhaylson@m2smart.com>
2021-11-07 12:07:30 -0400

    Verificando conexão
    Instalada lib NetInfo que permite verificar o status da conexão.
    
    Signed-off-by: Mhaylson Whandson <mhaylson@m2smart.com>

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import { StatusBar, StyleSheet, BackHandler } from 'react-native';
+import { StatusBar, StyleSheet, BackHandler, Alert } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { useTheme } from 'styled-components';
 import { RFValue } from 'react-native-responsive-fontsize';
+import NetInfo, { useNetInfo } from '@react-native-community/netinfo';
 import { RectButton, PanGestureHandler } from 'react-native-gesture-handler';
 
 import Animated, {
@@ -32,7 +33,7 @@ export function Home() {
     const [cars, setCars] = useState<CarDTO[]>();
     const [isLoading, setIsLoading] = useState(true);
 
-    const theme = useTheme();
+    const netInfo = useNetInfo();
     const navigation = useNavigation();
 
     const positionX = useSharedValue(0);
@@ -101,6 +102,14 @@ export function Home() {
             isMounted = false;
         }
     }, []);
+
+    useEffect(() => {
+        if (netInfo.isConnected) {
+            Alert.alert('Você está On-Line')
+        } else {
+            Alert.alert('Você está Off-Line')
+        }
+    }, [netInfo.isConnected]);
 
     // useEffect(() => {
 
